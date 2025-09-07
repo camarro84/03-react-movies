143f569dbcd107d2cde3a0b6c2bde1237976c2e4
camarro84 <camarro84@gmail.com>
2025-09-07 14:22:08 +0200

    change location of files

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "modern-normalize/modern-normalize.css";
 import "./styles/index.css";
-import App from "./App";
+import App from "./components/App/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
